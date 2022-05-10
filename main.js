c117e83b4827fe1096e8f099d4e8cb35971e9c2c
Xavier <dejuan.xavier@gmail.com>
2022-05-10 10:04:20 +0200

    Meilleure lisibilté du code

--- main.js
+++ main.js
@@ -1,84 +1,82 @@
-// ===== Get Beers (liste)
+  // ===== Get Beers (liste)
 
-// ===== Fonction asynchrone récupérant l'url après résolution de l'attente de la promesse du serveur.
-// ===== Stockée dans la variable response.
+  // ===== Fonction asynchrone récupérant l'url après résolution de l'attente de la promesse du serveur.
+  // ===== Stockée dans la variable response.
 async function getBeers() {
     try {
       const response = await axios.get('https://api.punkapi.com/v2/beers/');
-      // ===== Retourne la valeur response avec uniquement les informations contenues dans le data. 
+        // ===== Retourne la valeur response avec uniquement les informations contenues dans le data. 
       return response.data;
     } 
     catch (error) {
       console.log(error);
     }
-}
 
+  } 
 
-// ===== Récupération de la fonction dans la variable beers.
-// ===== Le tableau des bières est à présent disponible pour utilisation.
+  // ===== Récupération de la fonction dans la variable beers.
+  // ===== Le tableau des bières est à présent disponible pour utilisation.
 const beers = await getBeers();
+  
 
-
-
-// ===== Fonction qui devrait permettre d'afficher sous forme de cards les bières avec les détails choisis (nom, petite présentation, image, etc).
+  // ===== Fonction qui devrait permettre d'afficher sous forme de cards les bières avec les détails choisis (nom, petite présentation, image, etc).
 function beersShow() {
 
-// ===== Variable récupérant dans l'html toute la ligne ou se trouve "cardModal".
-let cardHtml = document.querySelector("#cardModal");
+      // ===== Variable récupérant dans l'html toute la ligne et tous les tags ou se trouve "cardModal".
+    let cardHtml = document.querySelector("#cardModal");
 
-// ===== Variable prenant en compte le clique sur le bouton détails.
-let details = document.addEventListener ("click", function () {
-  console.log(details)
-});
+      // ===== Variable prenant en compte le clique sur le bouton détails.
+    let details = document.addEventListener ("click", function () {
+      console.log(details)
+    });
 
-// ===== Création d'une boucle array qui parcours le array beers.
-  for(let beer of beers) {
-    console.log(beer);
-    console.log(beers)
-    
-    // ===== Création supposée des cards se générant automatiquement selon le Array beers ex: 10 bières ou 25 bières comme cet exercice.
-    // ===== AppendChild permet de lier la div ou tag parent à la div ou tag enfant.
-    let colDiv = document.createElement("div");
-    colDiv.classList = "col-2";
-      cardHtml.appendChild(colDiv);
+      // ===== Création d'une boucle array qui parcours le array beers.
+    for(let beer of beers) {
+      
+          // ===== Création supposée des cards se générant automatiquement selon le Array beers ex: 10 bières ou 25 bières comme cet exercice.
+          // ===== AppendChild permet de lier la div ou tag parent à la div ou tag enfant.
+        let colDiv = document.createElement("div");
+        colDiv.classList = "col-2";
+          cardHtml.appendChild(colDiv);
 
-    let cardDiv = document.createElement("div");
-    cardDiv.classList = "card";
-    cardDiv.setAttribute = ("id", "getBeers");
-      colDiv.appendChild(cardDiv);
+        let cardDiv = document.createElement("div");
+        cardDiv.classList = "card";
+        cardDiv.setAttribute = ("id", "getBeers");
+          colDiv.appendChild(cardDiv);
 
-    let img = document.createElement("img");
-    img.classList = "card-img-top";
-    img.setAttribute = ("id", "image")
-    img.src = beer.image_url;
-      cardDiv.appendChild(img);
+        let img = document.createElement("img");
+        img.classList = "card-img-top";
+        img.setAttribute = ("id", "image")
+        img.src = beer.image_url;
+          cardDiv.appendChild(img);
 
-    let cardBodyDiv = document.createElement("div");
-    cardBodyDiv.classList = "card-body";
-      img.appendChild(cardBodyDiv);
+        let cardBodyDiv = document.createElement("div");
+        cardBodyDiv.classList = "card-body";
+          img.appendChild(cardBodyDiv);
 
-    let cardTitle = document.createElement("h5");
-    cardTitle.classList = "card-title";
-    cardTitle.setAttribute = ("id", "nom");
-    cardTitle.innerHTML = beer.name;
+        let cardTitle = document.createElement("h5");
+        cardTitle.classList = "card-title";
+        cardTitle.setAttribute = ("id", "nom");
+        cardTitle.innerHTML = beer.name;
 
-    let sloganDiv = document.createElement("p");
-    sloganDiv.classList = "card-text";
-    sloganDiv.setAttribute = ("id", "slogan");
-    sloganDiv.innerHTML = beer.tagline;
+        let sloganDiv = document.createElement("p");
+        sloganDiv.classList = "card-text";
+        sloganDiv.setAttribute = ("id", "slogan");
+        sloganDiv.innerHTML = beer.tagline;
 
-    let modalDiv = document.createElement("a");
-    modalDiv.classList = "btn btn-primary";
-    modalDiv.setAttribute = ("id", "details");
-      cardBodyDiv.appendChild(modalDiv);
-      cardBodyDiv.appendChild(sloganDiv);
-      cardBodyDiv.appendChild(cardTitle);  
-   
-    // return cardHtml;
+        let modalDiv = document.createElement("a");
+        modalDiv.classList = "btn btn-primary";
+        modalDiv.setAttribute = ("id", "details");
+          cardBodyDiv.appendChild(modalDiv);
+          cardBodyDiv.appendChild(sloganDiv);
+          cardBodyDiv.appendChild(cardTitle);  
+      
+        // return cardHtml;
 
   }  
 }
-// ===== Appel de la fonction qui sans son appel rien ne s'affiche. 
+
+  // ===== Appel de la fonction qui sans son appel rien ne s'affiche. 
 beersShow();
 
 
